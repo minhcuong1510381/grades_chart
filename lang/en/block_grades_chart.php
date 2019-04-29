4b11d4b9b864ddf4714b5c543f7ec40de073180a
Minh Cuong <1510381@hcmut.edu.vn>
2019-04-30 02:07:22 +0700

    Update name plugin

--- lang/en/block_grades_chart.php
+++ lang/en/block_grades_chart.php
@@ -1,6 +1,6 @@
 <?php
-$string['pluginname'] = 'Analytics Grades';
-$string['grades_chart'] = 'Analytics Grades';
+$string['pluginname'] = 'E-learning Help And Assess Tool';
+$string['grades_chart'] = 'E-learning Help And Assess Tool';
 $string['grades_chart:addinstance'] = 'Add a new Analytics Grades block';
 $string['grades_chart:myaddinstance'] = 'Add a Analytics Grades block to the My Moodle page';
 $string['grades_chart:bemonitored'] = 'User is monitored in the course';
